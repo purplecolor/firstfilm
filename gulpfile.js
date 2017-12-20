1a766eb61c7f7b42ba5c1dc9e219fc818e8594cf
yuang <1157962772@qq.com>
2017-12-20 19:58:27 +0800

    我也不知道添加了什么

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
 var gulp=require("gulp");
+var sass=require("gulp-sass");
 gulp.task("hello",function(){
 	console.log("hello world");
+});
+gulp.task("copy-homepage",function(){
+	return gulp.src("homepage.html").pipe(gulp.dest("dist"));
+});
+gulp.task("sass",function(){
+	gulp.src("scss/homepage.scss").pipe(sass()).pipe(gulp.dest("dist/css"));
+	gulp.src("scss/homepage.scss").pipe(sass()).pipe(gulp.dest("css"));
+});
+gulp.task("watch",function(){
+	gulp.watch("scss/homepage.scss",["sass"]);
+	gulp.watch("homepage.html",["copy-homepage"]);
 })
+
